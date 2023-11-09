f4fe582e88a1276ba5d0f8cc1c1f7e08ead3762a
AayushAgrawal2003 <aayush.agrawal2003@gmail.com>
2023-11-09 23:04:50 +0530

    distance filtering

---
+++ scripts/depth_filtering.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import numpy as np 
+import rospy 
+
+
+
+# Subsribe to the depth image 
+# Subscribe to the RGB image
+# Run the model on the RGB imag
+# Filter the depth using the semseg
+# Find the max and min using the depth image
