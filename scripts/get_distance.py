0cfaec3f04655acaa5f6f45bdd1d1165f7b1ef2f
AayushAgrawal2003 <aayush.agrawal2003@gmail.com>
2023-11-10 12:15:36 +0530

    completed the filter

--- scripts/get_distance.py
+++ scripts/get_distance.py
@@ -3,24 +3,60 @@ import rospy
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
 import numpy as np
+import cv2
 
-def convert_depth_image(ros_image):
-    bridge = CvBridge()
-    # Use cv_bridge() to convert the ROS image to OpenCV 
-    #Convert the depth image using the default passthrough encoding
-    depth_image = bridge.imgmsg_to_cv2(ros_image, desired_encoding="passthrough")
-    depth_array = np.array(depth_image, dtype=np.float32)
-    depth_image = bridge.imgmsg_to_cv2(ros_image, "passthrough")
-    depth_array = np.array(depth_image, dtype=np.float32)
-    print ('center depth:', depth_array[240,140])
-    print(depth_array)
-    # print 'center depth:', depth_array[[cy], [cx]]
-    # rospy.loginfo(depth_array)
+ITR_DENSITY = 10
 
-def pixel2depth():
-	rospy.init_node('pixel2depth',anonymous=True)
-	rospy.Subscriber("/depth", Image,callback=convert_depth_image, queue_size=1)
-	rospy.spin()
+
+class Depth():
+
+    def __init__(self):
+        self.body_max = -1
+        self.body_min = 2000
+        self.rate = rospy.Rate(1)
+        self.depth_array = []
+    def pitcure_iter(self):
+        while not rospy.is_shutdown():
+            self.pixel2depth()
+            image = cv2.imread("/home/inspire_01/catkin_ws/src/surface_pc/data/test_images/main.png")
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            rospy.loginfo(str(self.body_max) + " " +str(self.body_min))
+            # cv2.imshow("test" , gray)
+            # cv2.waitKey(0)
+            self.temp_max = -1
+            self.temp_min = 2000
+            dims = gray.shape   
+            if len(self.depth_array) > 0:
+                for i in range(0,dims[0],ITR_DENSITY):
+                    for j in range(0,dims[1],ITR_DENSITY):
+                        if gray[i][j] == 253:
+                            if self.depth_array[i][j] >  self.temp_max: 
+                                self.temp_max = self.depth_array[i][j]
+                            if self.depth_array[i][j] <  self.temp_min: 
+                                self.temp_min = self.depth_array[i][j] 
+                
+                self.body_max = self.temp_max
+                self.body_min = self.temp_min
+            else:
+                rospy.loginfo("No Depth Data")
+
+            self.rate.sleep()
+
+    def convert_depth_image(self,ros_image):
+        bridge = CvBridge()
+        # Use cv_bridge() to convert the ROS image to OpenCV 
+        #Convert the depth image using the default passthrough encoding
+        depth_image = bridge.imgmsg_to_cv2(ros_image, desired_encoding="passthrough")
+        depth_array = np.array(depth_image, dtype=np.float32)
+        depth_image = bridge.imgmsg_to_cv2(ros_image, "passthrough")
+        self.depth_array = np.array(depth_image, dtype=np.float32)
+        # print ('center depth:', depth_array[x,y])
+        # print(depth_array)
+
+    def pixel2depth(self):
+        rospy.Subscriber("/depth", Image,callback=self.convert_depth_image, queue_size=1)
 
 if __name__ == '__main__':
-	pixel2depth()
+    rospy.init_node("AdaptiveDepth")
+    new = Depth()
+    new.pitcure_iter()
